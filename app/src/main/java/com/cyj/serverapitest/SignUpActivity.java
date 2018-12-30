8799208707efddb6da93046ed0ebcecfa71886e4
test <test1>
2018-12-30 12:27:04 +0900

    회원가입 그림그리기

---
+++ app/src/main/java/com/cyj/serverapitest/SignUpActivity.java
@@ -0,0 +1,32 @@
+package com.cyj.serverapitest;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SignUpActivity extends BaseActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sign_up);
+
+        bindViews();
+        setupEvents();
+        setValues();
+    }
+
+    @Override
+    public void setupEvents() {
+
+    }
+
+    @Override
+    public void setValues() {
+
+    }
+
+    @Override
+    public void bindViews() {
+
+    }
+}
