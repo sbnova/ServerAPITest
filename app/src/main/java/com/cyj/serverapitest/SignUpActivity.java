99757b6934144d06f1c78a4c82099c184d71f5fc
test <test1>
2018-12-30 14:18:28 +0900

    회원가입 JSON 파싱 시작

--- app/src/main/java/com/cyj/serverapitest/SignUpActivity.java
+++ app/src/main/java/com/cyj/serverapitest/SignUpActivity.java
@@ -1,10 +1,26 @@
 package com.cyj.serverapitest;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
+
+import com.cyj.serverapitest.utils.PasswordUtil;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.IOException;
+
+import okhttp3.Call;
+import okhttp3.Callback;
+import okhttp3.FormBody;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.RequestBody;
+import okhttp3.Response;
 
 public class SignUpActivity extends BaseActivity {
 
@@ -32,6 +48,7 @@ public class SignUpActivity extends BaseActivity {
         singUpBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+
 //                몇가지 검증 절차.
                 if(userIdEdt.getText().toString().equals("")){
                     Toast.makeText(mContext, "아이디를 입력하세요.", Toast.LENGTH_SHORT).show();
@@ -60,6 +77,45 @@ public class SignUpActivity extends BaseActivity {
                     Toast.makeText(mContext, "비밀번호가 서로 다릅니다.", Toast.LENGTH_SHORT).show();
                     return;
                 }
+
+                OkHttpClient okHttpClient = new OkHttpClient();
+
+                RequestBody requestBody = new FormBody.Builder().add("user_id", userIdEdt.getText().toString())
+                        .add("password", PasswordUtil.getEncrytedPassword(userPwEdt.getText().toString()))
+                        .add("name", userNameEdt.getText().toString())
+                        .add("phone", userPhoneEdt.getText().toString())
+                        .add("email", userEmailEdt.getText().toString()).build();
+
+                Request request = new Request.Builder().url("http://api-dev.lebit.kr/auth").put(requestBody).build();
+
+                okHttpClient.newCall(request).enqueue(new Callback() {
+                    @Override
+                    public void onFailure(Call call, IOException e) {
+                        runOnUiThread(new Runnable() {
+                            @Override
+                            public void run() {
+                                Toast.makeText(mContext, "서버 통신실패", Toast.LENGTH_SHORT).show();
+                            }
+                        });
+                    }
+
+                    @Override
+                    public void onResponse(Call call, Response response) throws IOException {
+                        String responseBody = response.body().string();
+                        Log.d("회원가입리스폰스",responseBody);
+
+                        try {
+                            JSONObject root = new JSONObject(responseBody);
+                            int code = root.getInt("code");
+                            String message = root.getString("message");
+
+                            Log.d("회원가입리스폰스","코드 : " + code);
+                            Log.d("회원가입리스폰스","메세지 : " + message);
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                });
             }
         });
 
